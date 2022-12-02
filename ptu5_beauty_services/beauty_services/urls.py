e1637be3ff1e0505ea984005de421f478b2d5e20
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-12-02 16:55:17 +0200

    index changed, salons.html, salon_detail.html, services_list.html added

--- ptu5_beauty_services/beauty_services/urls.py
+++ ptu5_beauty_services/beauty_services/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('salons/', views.salons, name='salons'),
-    path('salon/<int:pk>/', views.SalondetailView.as_view(), name='salon'),
+    path('salon/<int:pk>/', views.SalonDetailView.as_view(), name='salon'),
     path('services/', views.ServiceListView.as_view(), name='services'),
 ]
--- ptu5_beauty_services/beauty_services/views.py
+++ ptu5_beauty_services/beauty_services/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
-from . models import ServiceType, BeautySalon
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from . models import ServiceType, BeautySalon, Service
+
 
 def index(request):
     types =  ServiceType.objects.all()
-    context = {'service_type': " | ".join(str(t) for t in types)}
+    salon_count = BeautySalon.objects.count()
+    service_count = Service.objects.count()
+    context = {
+        'service_type': " | ".join(str(t) for t in types),
+        'salon_count': salon_count,
+        'service_count': service_count,
+    }
     return render(request, 'beauty_services/index.html', context)
+
+def salons(request):
+    paginator = Paginator(BeautySalon.objects.all(), 15)
+    page_number = request.GET.get('page')
+    paged_salons = paginator.get_page(page_number)
+    return render(request, 'beauty_services/salons.html', {'salons': paged_salons})
+
+
+class SalonDetailView(DetailView):
+    model = BeautySalon
+    template_name = 'beauty_services/salon_detail.html'
+
+# def salon(request, salon_id):
+#     context = {
+#         'salon': get_object_or_404(BeautySalon, id=salon_id),
+#     }
+#     return render(request, 'beauty_services/salon.html', context)
+
+class ServiceListView(ListView):
+    model = Service
+    paginate_by = 15
+    template_name = 'beauty_services/services_list.html'
