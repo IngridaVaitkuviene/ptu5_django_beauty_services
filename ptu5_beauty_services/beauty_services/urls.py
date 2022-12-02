2c3fb6933cf1336cc8b2b336a450af7f51d059c6
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-12-02 16:46:52 +0200

    salons, salon & services url added

--- ptu5_beauty_services/beauty_services/urls.py
+++ ptu5_beauty_services/beauty_services/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('salons/', views.salons, name='salons'),
+    path('salon/<int:pk>/', views.SalondetailView.as_view(), name='salon'),
+    path('services/', views.ServiceListView.as_view(), name='services'),
 ]
