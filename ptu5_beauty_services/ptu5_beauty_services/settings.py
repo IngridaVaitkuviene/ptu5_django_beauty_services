beb5bb346d6886fec2d2fd45780c30fd12fc7138
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-12-04 15:17:13 +0200

    user_profile app added, settings, urls

--- ptu5_beauty_services/ptu5_beauty_services/settings.py
+++ ptu5_beauty_services/ptu5_beauty_services/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'beauty_services',
+    'user_profile',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,6 +128,8 @@ MEDIA_ROOT = BASE_DIR.joinpath('media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
 
 TINYMCE_DEFAULT_CONFIG = {
     'height': 400,
--- ptu5_beauty_services/ptu5_beauty_services/urls.py
+++ ptu5_beauty_services/ptu5_beauty_services/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('beauty_services.urls')),
     path('tinymce', include('tinymce.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('user_profile/', include('user_profile.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
