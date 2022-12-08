2a29167e662852813e7b61a444e91384ee8c5be6
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-12-08 15:45:56 +0200

    translations settings added

--- ptu5_beauty_services/ptu5_beauty_services/urls.py
+++ ptu5_beauty_services/ptu5_beauty_services/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('', include('beauty_services.urls')),
     path('tinymce', include('tinymce.urls')),
